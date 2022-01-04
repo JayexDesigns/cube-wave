c69e5cd9b3147a960a98e85d23496ff0d954a266
Jayex <joaquintouron@gmail.com>
2022-01-04 19:05:42 +0100

    added more controller options

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-var scene = new THREE.Scene();
+// Scene And Default Background Color
+const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x8f91fe);
 
 
 
-var persCamera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth/window.innerHeight,
-    0.01,
-    1000
-);
-
-persCamera.position.set(5,5,5);
-persCamera.rotation.set(0,0,0);
-persCamera.lookAt(scene.position);
 
 
-
+// Orthographic Camera, It Has No Perspective, Great For Isometric Projections
 const ortCameraDivider = 180;
-var ortCamera = new THREE.OrthographicCamera(
+const ortCamera = new THREE.OrthographicCamera(
     -window.innerWidth/ortCameraDivider,
     window.innerWidth/ortCameraDivider,
     window.innerHeight/ortCameraDivider,
@@ -32,7 +23,8 @@ ortCamera.lookAt(scene.position);
 
 
 
-var renderer = new THREE.WebGLRenderer({antialias: true});
+// Renderer Initialization
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -40,8 +32,6 @@ document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth,window.innerHeight);
-    persCamera.aspect = window.innerWidth / window.innerHeight;
-    persCamera.updateProjectionMatrix();
     ortCamera.left = -window.innerWidth/ortCameraDivider;
     ortCamera.right = window.innerWidth/ortCameraDivider;
     ortCamera.top = window.innerHeight/ortCameraDivider;
@@ -51,16 +41,9 @@ window.addEventListener('resize', () => {
 
 
 
-var light = new THREE.DirectionalLight(0xffffff, 1.6);
-light.position.set(2, 3, 1);
-light.target = scene;
-scene.add(light);
 
 
-
-
-
-//Shaders
+// Shaders, Just Puts Each Color In The Respective Face
 const _VS = `
 varying vec3 v_Normal;
 
@@ -90,54 +73,74 @@ void main () {
 
 
 
-var boxSize = 0.4;
-var rows = 15;
-var cols = 15;
+// Mesh Building Variables
+let boxSize = 0.4;
+let rows = 15;
+let cols = 15;
 
-var boxes = [];
+let color1 = [255, 255, 255];
+let color2 = [218, 218, 218];
+let color3 = [109, 109, 109];
 
-var geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
-var material = new THREE.ShaderMaterial({
-    uniforms: {
-        modelColor1: {value: new THREE.Vector3(0.8549019607843137, 0.8549019607843137, 0.8549019607843137)},
-        modelColor2: {value: new THREE.Vector3(1, 1, 1)},
-        modelColor3: {value: new THREE.Vector3(0.4274509803921568, 0.4274509803921568, 0.4274509803921568)},
+let boxes = [];
+
+let geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+let material = new THREE.ShaderMaterial({
+    uniforms: { // Default Shader Colors
+        modelColor1: {value: new THREE.Vector3(color2[0]/255, color2[1]/255, color2[2]/255)},
+        modelColor2: {value: new THREE.Vector3(color1[0]/255, color1[1]/255, color1[2]/255)},
+        modelColor3: {value: new THREE.Vector3(color3[0]/255, color3[1]/255, color3[2]/255)},
     },
     vertexShader: _VS,
     fragmentShader: _FS,
 });
-// var material = new THREE.MeshLambertMaterial({color:0xffffff}); // Old Simple Color Without Shader
-for (let i = 0; i < rows; ++i) {
-    boxes.push([]);
-    for (let j = 0; j < cols; ++j) {
-        let box = new THREE.Mesh(geometry, material);
-        box.position.set(
-            -(rows*boxSize/2) + boxSize/2 + i*boxSize,
-            0,
-            -(cols*boxSize/2) + boxSize/2 + j*boxSize
-        );
-        scene.add(box);
-        boxes[i].push(box);
+
+const createBoxes = () => {
+    for (let i = 0; i < rows; ++i) {
+        boxes.push([]);
+        for (let j = 0; j < cols; ++j) {
+            let box = new THREE.Mesh(geometry, material);
+            box.position.set(
+                -(rows*boxSize/2) + boxSize/2 + i*boxSize,
+                0,
+                -(cols*boxSize/2) + boxSize/2 + j*boxSize
+            );
+            scene.add(box);
+            boxes[i].push(box);
+        }
     }
-}
+};
+
+const deleteBoxes = () => {
+    for (let i = 0; i < boxes.length; ++i) {
+        for (let j = 0; j < boxes[i].length; ++j) {
+            scene.remove(boxes[i][j]);
+        }
+    }
+    boxes = [];
+};
+
+createBoxes();
 
 
 
-var angle = 0;
-var minH = 2;
-var maxH = 7;
-var speed = 0.065;
-var scale = 2.75;
-var stopped = false;
+// Animation Variables
+let angle = 0;
+let minH = 2;
+let maxH = 7;
+let speed = 0.065;
+let offset = 2.75;
+let stopped = false;
 
-var render = function() {
+// Render Loop
+let render = function() {
     requestAnimationFrame(render);
     renderer.render(scene, ortCamera);
     if (!stopped) {
         for (let i = 0; i < boxes.length; ++i) {
             for (let j = 0; j < boxes[i].length; ++j) {
                 let distance = boxes[i][j].position.x**2 + boxes[i][j].position.z**2;
-                distance = -(distance / scale);
+                distance = -(distance / offset);
                 boxes[i][j].scale.y = ((Math.sin(angle + distance)+1) * (maxH - minH)) + minH;
             }
         }
@@ -154,26 +157,26 @@ render();
 
 const controller = {
     changeBoxColor: (newColor1, newColor2, newColor3) => {
-        newColor1 = [
+        color1 = [
             parseInt(newColor1.substr(0, 2), 16),
             parseInt(newColor1.substr(2, 2), 16),
             parseInt(newColor1.substr(4, 2), 16),
         ];
-        newColor2 = [
+        color2 = [
             parseInt(newColor2.substr(0, 2), 16),
             parseInt(newColor2.substr(2, 2), 16),
             parseInt(newColor2.substr(4, 2), 16),
         ];
-        newColor3 = [
+        color3 = [
             parseInt(newColor3.substr(0, 2), 16),
             parseInt(newColor3.substr(2, 2), 16),
             parseInt(newColor3.substr(4, 2), 16),
         ];
         for (let i = 0; i < boxes.length; ++i) {
             for (let j = 0; j < boxes[i].length; ++j) {
-                boxes[i][j].material.uniforms.modelColor1.value = new THREE.Vector3(newColor2[0]/255, newColor2[1]/255, newColor2[2]/255);
-                boxes[i][j].material.uniforms.modelColor2.value = new THREE.Vector3(newColor1[0]/255, newColor1[1]/255, newColor1[2]/255);
-                boxes[i][j].material.uniforms.modelColor3.value = new THREE.Vector3(newColor3[0]/255, newColor3[1]/255, newColor3[2]/255);
+                boxes[i][j].material.uniforms.modelColor1.value = new THREE.Vector3(color2[0]/255, color2[1]/255, color2[2]/255);
+                boxes[i][j].material.uniforms.modelColor2.value = new THREE.Vector3(color1[0]/255, color1[1]/255, color1[2]/255);
+                boxes[i][j].material.uniforms.modelColor3.value = new THREE.Vector3(color3[0]/255, color3[1]/255, color3[2]/255);
                 // boxes[i][j].material.color.setHex(color); // For Old Simple Color Without Shader
             }
         }
@@ -207,11 +210,53 @@ const controller = {
         },
     ],
 
+    changeMeshBuilding: (callback) => {
+        stopped = true;
+        deleteBoxes();
+        callback();
+        createBoxes();
+        stopped = false;
+    },
+
+    changeBoxSize: (size) => controller.changeMeshBuilding(() => {
+        boxSize = size;
+        geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+    }),
+
+    changeRows: (r) => controller.changeMeshBuilding(() => rows = r),
+
+    changeCols: (c) => controller.changeMeshBuilding(() => cols = c),
+
+    changeMaxHeight: (h) => maxH = h,
+
+    changeMinHeight: (h) => minH = h,
+
+    changeSpeed: (s) => speed = s,
+
+    changeOffset: (o) => offset = o,
+
+    stop: () => stopped = !stopped,
+
     resetDefaultValues: () => {
-        controller.palettes[0]();
+        controller.changeBoxColor("ffffff", "dadada", "6d6d6d");
+        controller.changeSceneColor("8f91fe");
+        controller.changeBoxSize(0.4);
+        controller.changeRows(15);
+        controller.changeCols(15);
+        controller.changeMaxHeight(7);
+        controller.changeMinHeight(2);
+        controller.changeSpeed(0.065);
+        controller.changeOffset(2.75);
     },
 };
 
 
 
+// Starts With The Default Values
 controller.resetDefaultValues();
+
+
+// TODO
+// Delta Time And Frames
+// Wallpaper Engine Connection
+// Improve Color Functions
